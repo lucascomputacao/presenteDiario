fa99c6537fbdff1308ae699d5cb1567cb011bf28
lucascomputacao <lucascomputacao@gmail.com>
2015-08-13 16:36:01 -0300

    Retirando bug de Provider e atualizando código

--- app/src/main/java/com/lucasborgesdev/presentediario/MainActivity.java
+++ app/src/main/java/com/lucasborgesdev/presentediario/MainActivity.java
@@ -1,7 +1,6 @@
 package com.lucasborgesdev.presentediario;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,7 +8,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,25 +15,9 @@ import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.webkit.URLUtil;
 import android.widget.Button;
-import android.widget.ShareActionProvider;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
