dbdfaf73685ad41b13184fc3d89248951315b2c4
lucascomputacao <lucascomputacao@gmail.com>
2015-08-14 22:23:31 -0300

    Removido download de arquivo de áudio em ShareAudioActivity

--- app/src/main/java/com/lucasborgesdev/presentediario/ShareAudioActivity.java
+++ app/src/main/java/com/lucasborgesdev/presentediario/ShareAudioActivity.java
@@ -23,41 +23,14 @@ public class ShareAudioActivity extends Activity {
 
         super.onCreate(savedInstanceState);
 
-
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         SimpleDateFormat sdfNoTrace = new SimpleDateFormat("ddMMyyyy");
         Calendar cal = Calendar.getInstance();
         Date now = cal.getTime(); // set the current datetime in a Date-object
         final String dateFormatTraces = sdf.format(now); // contains dd-MM-yyyy (e.g. 15-03-2015 for March 15, 2015)
-        // making URLs
-        final String url_text_redirect = "http://www.transmundial.org.br/presente-diario/" + dateFormatTraces;
         String dateFormatNoTraces = sdfNoTrace.format(now); // contains dd-MM-yyyy (e.g. 15-03-2015 for March 15, 2015)
-        // Calculando a versão
-        int year = cal.get(Calendar.YEAR); // get the current year
-        int norma = 1997;
-        int versao = year - norma;
-        final String url_audio_redirect = "http://transmundial.org.br/podcast/presente_diario/" + versao
-                + "/" + "presente" + dateFormatNoTraces + ".mp3";
-
-        final String title_download_audio = "Presente_Diário_" + dateFormatTraces + "."
-                + MimeTypeMap.getFileExtensionFromUrl(url_audio_redirect);
 
         try {
-            // Download do arquivo de Áudio
-            String nameOfFile = URLUtil.guessFileName(url_audio_redirect, null,
-                    MimeTypeMap.getFileExtensionFromUrl(url_audio_redirect));
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url_audio_redirect));
-            request.setTitle(title_download_audio);
-            String description = "Áudio Presente Diário " + dateFormatTraces;
-            request.setDescription(description);
-            // use a linha abaixo se quiser limitar o download por wifi / tem opção de dados tbm
-            //request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            request.setDestinationInExternalPublicDir("/PresenteDiario", nameOfFile);
-            DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            manager.enqueue(request);
-
             // Intent compartilhar audio
             Intent shareAudio = new Intent(Intent.ACTION_SEND);
             shareAudio.setType("audio/mp3");
